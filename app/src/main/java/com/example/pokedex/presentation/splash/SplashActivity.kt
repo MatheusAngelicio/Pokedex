cf1e77864571d9d229db7ac388581590fa662d96
Matheus Angelicio <matheus.angelicio00@gmail.com>
2022-04-19 14:07:34 -0300

    Create Splash

--- app/src/main/java/com/example/pokedex/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/example/pokedex/presentation/splash/SplashActivity.kt
@@ -2,18 +2,25 @@ package com.example.pokedex.presentation.splash
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.example.pokedex.R
 import com.example.pokedex.databinding.ActivitySplashBinding
 import com.example.pokedex.presentation.base.BaseActivity
-import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseActivity<ActivitySplashBinding>(R.layout.activity_splash) {
 
-    private val viewModel: SplashViewModel by inject()
+    private val viewModel by viewModel<SplashViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        getRandomBackgroung()
+    }
+
+    private fun getRandomBackgroung() {
+        binding.splashBackground.background =
+            ContextCompat.getDrawable(this, viewModel.backgroundList.random())
     }
 }
--- app/src/main/java/com/example/pokedex/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/com/example/pokedex/presentation/splash/SplashViewModel.kt
@@ -1,6 +1,21 @@
 package com.example.pokedex.presentation.splash
 
 import androidx.lifecycle.ViewModel
+import com.example.pokedex.R
 
 class SplashViewModel: ViewModel() {
+
+    val backgroundList = intArrayOf(
+        R.drawable.charizard,
+        R.drawable.charizard2,
+        R.drawable.gengar,
+        R.drawable.galarian,
+        R.drawable.sylveon,
+        R.drawable.umbreon,
+        R.drawable.centiskorch,
+        R.drawable.rillaboom
+    )
+
+
+
 }
