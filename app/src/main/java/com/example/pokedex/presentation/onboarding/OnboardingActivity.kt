763d023a8a761e949f55db2580dc0401f5ef9174
Matheus Angelicio <matheus.angelicio00@gmail.com>
2022-04-20 10:18:16 -0300

    Add MediaPlayer

--- app/src/main/java/com/example/pokedex/presentation/onboarding/OnboardingActivity.kt
+++ app/src/main/java/com/example/pokedex/presentation/onboarding/OnboardingActivity.kt
@@ -2,13 +2,19 @@ package com.example.pokedex.presentation.onboarding
 
 import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.pokedex.R
 import com.example.pokedex.databinding.ActivityOnboardingBinding
 import com.example.pokedex.presentation.base.BaseActivity
 
-class OnboardingActivity: BaseActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
+class OnboardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
+
+    private var mp: MediaPlayer? = null
+    private var currentSong = mutableListOf(R.raw.pokemonmusic)
 
     companion object {
         fun getStartIntent(context: Context) = Intent(context, OnboardingActivity::class.java)
@@ -18,6 +24,23 @@ class OnboardingActivity: BaseActivity<ActivityOnboardingBinding>(R.layout.activ
         super.onCreate(savedInstanceState)
 
 
+        controlSound(currentSong[0])
 
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mp != null) mp?.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mp?.start()
+    }
+
+    private fun controlSound(id: Int) {
+        mp = MediaPlayer.create(this, id)
+        mp?.start()
+        mp?.isLooping = true
     }
 }
